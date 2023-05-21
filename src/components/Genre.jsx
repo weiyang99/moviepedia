3b5e9e9eb4657edbe7f3d13719135f28835bc8dd
weiyang <weiyangwygoh@gmail.com>
2023-05-21 17:44:56 +0800

    searchfeed + genre nav back by page

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Menu from './Menu'
 import { Box, FormControl, IconButton, InputLabel, NativeSelect, Pagination, Stack, Typography } from '@mui/material'
-import { Movie } from '@mui/icons-material'
+import { ArrowBack, Movie } from '@mui/icons-material'
 import { Link, useParams } from 'react-router-dom'
 import SearchBar from './SearchBar'
 import { fetchFromAPI } from './fetchFromAPI'
@@ -21,6 +21,11 @@ const Genre = () => {
         setPageNumber(p)
     }
 
+    const handlePage = (e) => {
+        e.preventDefault()
+        if (pageNumber > 1) { setPageNumber(pageNumber - 1) }
+    }
+
     const handleClick = (e) => {
         e.preventDefault()
         setFilter(e.target.value)
@@ -32,6 +37,8 @@ const Genre = () => {
 
         fetchFromAPI(`discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&sort_by=${filter}&page=${pageNumber}&with_genres=${id}`)
             .then((data) => setTotalPages(data.total_pages));
+
+        window.scrollTo(0, 0)
     }, [id, pageNumber, filter])
 
     useEffect(() => {
@@ -42,11 +49,11 @@ const Genre = () => {
         <>
             <Menu />
 
-            <Box
-                p='0 8%'
-                sx={{ flex: 2, height: movies.length < 8 ? '100vh' : 'fit-content', backgroundColor: '#191919' }}
-            >
-                <Stack direction='row' alignItems='center' justifyContent='center' pt={2.5}>
+            <Box>
+                <Stack className='fix' direction='row' alignItems='center' justifyContent='center' pt={2.5} sx={{ backgroundColor: '#191919' }}>
+                    <IconButton type='button' onClick={handlePage} sx={{ p: '10px', color: 'gold' }}>
+                        <ArrowBack fontSize='large' />
+                    </IconButton>
                     <Link to='/'>
                         <IconButton type='submit' sx={{ p: '10px', color: 'darkOrange' }}>
                             <Movie fontSize='large' />
@@ -55,52 +62,58 @@ const Genre = () => {
                     <SearchBar />
                 </Stack>
 
-                <Typography
-                    variant='h4'
-                    fontWeight='bold'
-                    pl={2}
-                    sx={{ color: 'white', borderLeft: '7px solid gold' }}
+                <Box
+                    p='10% 8% 0 8%'
+                    sx={{ flex: 2, height: movies.length < 8 ? '100vh' : 'fit-content', backgroundColor: '#191919' }}
                 >
-                    Genre: <span style={{ color: 'gold' }}>{genre}</span>
-                </Typography>
 
-                <Stack direction='row' justifyContent='space-between' alignItems='center' mb={10} mt={2}>
                     <Typography
                         variant='h4'
                         fontWeight='bold'
                         pl={2}
                         sx={{ color: 'white', borderLeft: '7px solid gold' }}
                     >
-                        Page: <span style={{ color: 'gold' }}>{pageNumber}</span>
+                        Genre: <span style={{ color: 'gold' }}>{genre}</span>
                     </Typography>
 
-                    <FormControl autoWidth>
-                        <InputLabel variant="standard" sx={{ color: 'orange', fontSize: '1.3rem', fontWeight: 'bold' }}>
-                            Sort By:
-                        </InputLabel>
-                        <NativeSelect
-                            defaultValue={'popularity.desc'}
-                            sx={{ color: 'white' }}
-                            onClick={handleClick}
+                    <Stack direction='row' justifyContent='space-between' alignItems='center' mb={10} mt={2}>
+                        <Typography
+                            variant='h4'
+                            fontWeight='bold'
+                            pl={2}
+                            sx={{ color: 'white', borderLeft: '7px solid gold' }}
                         >
-                            <option style={{ backgroundColor: '#121212' }} value={'popularity.desc'} >Popularity</option>
-                            <option style={{ backgroundColor: '#121212' }} value={'release_date.desc'} >Release Date</option>
-                            <option style={{ backgroundColor: '#121212' }} value={'vote_count.desc'} >Vote Count</option>
-                        </NativeSelect>
-                    </FormControl>
-                </Stack>
+                            Page: <span style={{ color: 'gold' }}>{pageNumber}</span>
+                        </Typography>
 
-                <Movies movies={movies} />
+                        <FormControl autoWidth>
+                            <InputLabel variant="standard" sx={{ color: 'orange', fontSize: '1.3rem', fontWeight: 'bold' }}>
+                                Sort By:
+                            </InputLabel>
+                            <NativeSelect
+                                defaultValue={'popularity.desc'}
+                                sx={{ color: 'white' }}
+                                onClick={handleClick}
+                            >
+                                <option style={{ backgroundColor: '#121212' }} value={'popularity.desc'} >Popularity</option>
+                                <option style={{ backgroundColor: '#121212' }} value={'release_date.desc'} >Release Date</option>
+                                <option style={{ backgroundColor: '#121212' }} value={'vote_count.desc'} >Vote Count</option>
+                            </NativeSelect>
+                        </FormControl>
+                    </Stack>
 
-                <Pagination
-                    count={totalPages}
-                    onChange={handleChange}
-                    page={pageNumber}
-                    color='primary'
-                    sx={{ margin: 'auto', alignItems: 'center', width: 'fit-content', backgroundColor: 'darkGrey', marginTop: '5em', borderRadius: '2em' }}
-                />
+                    <Movies movies={movies} />
 
-                <Footer />
+                    <Pagination
+                        count={totalPages}
+                        onChange={handleChange}
+                        page={pageNumber}
+                        color='primary'
+                        sx={{ margin: 'auto', alignItems: 'center', width: 'fit-content', backgroundColor: 'darkGrey', marginTop: '5em', borderRadius: '2em' }}
+                    />
+
+                    <Footer />
+                </Box>
             </Box >
         </>
     )
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { Box, IconButton, Typography, Stack, Pagination } from '@mui/material'
 import { Link, useParams } from 'react-router-dom'
-import { Movie } from '@mui/icons-material'
+import { ArrowBack, Movie } from '@mui/icons-material'
 
 import Movies from './Movies'
 import { fetchFromAPI } from './fetchFromAPI'
@@ -21,22 +21,30 @@ const SearchFeed = () => {
         setPageNumber(p)
     }
 
+    const handlePage = (e) => {
+        e.preventDefault()
+        if (pageNumber > 1) { setPageNumber(pageNumber - 1) }
+    }
+
     useEffect(() => {
         fetchFromAPI(`search/movie?api_key=${REACT_APP_API_KEY}&query=${searchTerm}&language=en-US&page=${pageNumber}`)
             .then((data) => setMovies(data.results));
 
         fetchFromAPI(`search/movie?api_key=${REACT_APP_API_KEY}&query=${searchTerm}&language=en-US&page=${pageNumber}`)
             .then((data) => setTotalPages(data.total_pages));
+
+        window.scrollTo(0, 0)
     }, [searchTerm, pageNumber])
 
     return (
         <>
             <Menu />
 
-            <Box
-                sx={{ flex: 2, height: movies.length < 8 ? '100vh' : 'fit-content', backgroundColor: '#191919' }}
-            >
+            <Box>
                 <Stack className='fix' direction='row' alignItems='center' justifyContent='center' pt={2.5} bgcolor='#191919'>
+                    <IconButton type='button' onClick={handlePage} sx={{ p: '10px', color: 'gold' }}>
+                        <ArrowBack fontSize='large' />
+                    </IconButton>
                     <Link to='/'>
                         <IconButton type='submit' sx={{ p: '10px', color: 'darkOrange' }}>
                             <Movie fontSize='large' />
@@ -45,15 +53,25 @@ const SearchFeed = () => {
                     <SearchBar />
                 </Stack>
 
-                <Box p='10% 8% 0 8%'>
+                <Box p='10% 8% 0 8%' sx={{ flex: 2, height: movies.length < 8 ? '100vh' : 'fit-content', backgroundColor: '#191919' }}>
                     <Typography
                         variant='h4'
                         fontWeight='bold'
-                        mb={10}
+                        mb={2}
                         pl={2}
                         sx={{ color: 'white', borderLeft: '7px solid gold' }}
                     >
                         Search Results for: <span style={{ color: 'gold' }}>{searchTerm}</span>
+                    </Typography>
+
+                    <Typography
+                        variant='h4'
+                        fontWeight='bold'
+                        pl={2}
+                        mb={10}
+                        sx={{ color: 'white', borderLeft: '7px solid gold' }}
+                    >
+                        Page: <span style={{ color: 'gold' }}>{pageNumber}</span>
                     </Typography>
 
                     <Movies movies={movies} />
